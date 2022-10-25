63cb867a516f01bfe7c57e5a0b727644202abc76
Николай Гаврилов <gr413_ganan@lab-407-6.tomtit-tomsk.ru>
2022-10-25 12:11:04 +0700

    Тут текст

---
+++ locallibrary/locallibrary/catalog/views.py
@@ -0,0 +1,52 @@
+from django.http import Http404
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    return render(
+        request,
+        'index.html',
+        context=
+        {'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available,
+         'num_authors': num_authors,
+         'num_visits': num_visits},
+    )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'catalog/book_list.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    paginate_by = 2
+
+
+def book_detail_view(request, pk):
+    try:
+        book_id = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        raise Http404("Book does not exist")
+
+    return render(
+        request,
+        'catalog/book_detail.html', context={'book': book_id, }
+    )
+
